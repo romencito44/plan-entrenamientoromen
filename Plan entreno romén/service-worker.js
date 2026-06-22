f314ba3ae85b0f1ceb552f6530b3ac25803872e8
Romén <romenra07@gmail.com>
2026-06-22 21:35:45 +0100

    Unifica el arreglo de tablas moviles: etiquetas dinamicas tambien en las tablas de microciclo (menu semanal, registro de cardio)

--- Plan entreno romén/service-worker.js
+++ Plan entreno romén/service-worker.js
@@ -2,7 +2,7 @@
 // Sube CACHE_NAME (ej. "plan-romen-v2") cada vez que cambies
 // el contenido de las páginas para forzar a los dispositivos a descargar
 // la versión nueva.
-const CACHE_NAME = "plan-romen-v6";
+const CACHE_NAME = "plan-romen-v7";
 
 const ARCHIVOS_CACHE = [
   "./",
